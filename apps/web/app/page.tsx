0c6335ffbbadfb4bcf116d1ebe992141e2ed62e2
johnkdunyo <johnkdunyo@gmail.com>
2023-07-10 17:46:12 +0000

    removed unused codes and styles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button, Header } from "shared-components";
+import { Header } from "shared-components";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
-      <Button />
     </>
   );
 }
